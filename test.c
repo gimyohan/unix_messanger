556b13110832402160c146735358084107507c34
javago <javago.cs@gmail.com>
2024-12-15 13:43:53 +0000

    fixed cs

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include "headers.h"
 
-#define MAX_MSQ_SIZE 10
+#define MAX_MSQ_SIZE 5
 #define MAX_BODY_SIZE (1<<10)
-#define MAX_TALKER_NUM 5
+#define MAX_TALKER_NUM 3
 
 struct message{
 	char body[MAX_BODY_SIZE];
@@ -27,6 +27,8 @@ int *shm_id, *shm_meta;
 	3. add[1] = {msg#, }
 */
 
+int send_delay, receive_delay;
+int debug_mode;
 int id, msg_idx;
 int sem; //sem[1 + MAX_TALKER_NUM + 3] = {e, n_1, n_2, n_3, ... n_N, id_set, msg#, msg.N}
 enum sem_num{
@@ -47,6 +49,28 @@ int semSignal(int num){
 	buf.sem_num = num;
 	return semop(sem, &buf, 1);
 }
+
+void __debug_show_shm(){
+   int i=0;
+   for(i=0;i<MAX_MSQ_SIZE;i++){
+      printf("===================\n");
+      printf("msg queue%d:\n", i);
+      printf("id:%d, ttl:%d\n", shm_msg[i].sender_id, shm_msg[i].time_to_live);
+      printf("body:%s\n", shm_msg[i].body);
+   }
+   printf("--------------------\n");
+
+   printf("current message pointer:%d\n", shm_meta[0]);
+   printf("current empty message:%d\n", semctl(sem, E, GETVAL));
+   printf("--------------------\n");
+
+   printf("id[%d]\n", shm_id[0]);
+   for(i=1;i<=MAX_TALKER_NUM;i++)printf("(%d) ", shm_id[i]);
+   printf("\n");
+   printf("--------------------\n");
+}
+
+
 void doSender(){
 	int number_of_talkers, idx, i;
 	int id_list[MAX_TALKER_NUM], is_quit=0;
@@ -55,6 +79,27 @@ void doSender(){
 		body_buffer[n-1] = 0;
 		
 		semWait(E);
+
+		/*1
+		semWait(IDX);
+
+		idx = *shm_meta;
+		strcpy(shm_msg[idx].body, body_buffer);
+		shm_msg[idx].sender_id = id;
+		shm_msg[idx].time_to_live = number_of_talkers = shm_id[0];
+		(*shm_meta)++;
+		if(*shm_meta==MAX_MSQ_SIZE) *shm_meta = 0;
+		idx = *shm_meta;
+
+		if(send_delay) sleep(send_delay);
+
+		semSignal(IDX);
+		
+		for(i=1;i<=MAX_TALKER_NUM;i++){
+			if(shm_id[i]) semSignal(i);
+		}
+		end*/
+		/*2*/
 		semWait(IDX);
 		idx = *shm_meta;
 		(*shm_meta)++;
@@ -65,16 +110,20 @@ void doSender(){
 		for(i=1, number_of_talkers=0;i<=MAX_TALKER_NUM;i++){
 			if(shm_id[i]) id_list[number_of_talkers++] = i;
 		}
-		shm_msg[idx].time_to_live = number_of_talkers = shm_id[0];
+		shm_msg[idx].time_to_live = number_of_talkers;
 		for(i=0;i<number_of_talkers;i++) semSignal(id_list[i]);
+		if(send_delay) sleep(send_delay);
 		if(strcmp(body_buffer, "talk_quit")==0){
 			is_quit = 1;
 			semWait(ID);
-			shm_id[0]--; //after enter "talk_quit" do not receive any other messages immediately
-	        shm_id[id] = 0;
+			shm_id[0]--;
+			shm_id[id] = 0;
 			semSignal(ID);
 		}
 		semSignal(IDX);
+
+		/*end*/
+		if(debug_mode)__debug_show_shm();
 		
 		if(number_of_talkers==1){
 			printf("id=%d, talkers=%d, msg#=%d\n", id, number_of_talkers, ++idx==MAX_MSQ_SIZE?0:idx);
@@ -102,6 +151,7 @@ void doReceiver(){
 		ttl = shm_msg[msg_idx].time_to_live;
 
 		semSignal(MSG);
+		if(receive_delay)sleep(receive_delay);
 
 		if(ttl==0) semSignal(E);
 		if(id==sender_id&&strcmp(body_buffer, "talk_quit")==0) break;
@@ -171,6 +221,13 @@ void clear(){
 int main(int argc, char **argv){
 
 	if(argc==1)return 1; id = atoi(argv[1]);
+	if(argc>=3&&strcmp(argv[2], "-d")==0){
+		send_delay = receive_delay = 1;
+		if(argc>=4)send_delay = atoi(argv[3]);
+		if(argc>=5)receive_delay = atoi(argv[4]);
+		printf("send delay: %d, receive delay: %d\n", send_delay, receive_delay);
+	}
+	if(id==1)debug_mode = 1;
 
 	init();
 
